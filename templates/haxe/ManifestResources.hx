b91b87b2add75c616fa57a382f8220c952e33ebe
Robert Konrad <git2016@robdangero.us>
2018-05-04 09:10:50 -0700

    Remove prebaked build files

--- templates/haxe/ManifestResources.hx
+++ templates/haxe/ManifestResources.hx
@@ -54,17 +54,13 @@ import sys.FileSystem;
 		
 		var data, manifest, library;
 		
-		::if (assets != null)::::foreach assets::::if (type == "manifest")::::if (embed)::data = '::data::';
-		manifest = AssetManifest.parse (data, rootPath);
+		::manifest::
 		library = AssetLibrary.fromManifest (manifest);
 		Assets.registerLibrary ("::library::", library);
-		::else::Assets.libraryPaths["::library::"] = rootPath + "::resourceName::";
-		::end::::end::::end::::end::
 		
-		::foreach libraries::::if (preload)::library = Assets.getLibrary ("::name::");
+		
 		if (library != null) preloadLibraries.push (library);
-		else preloadLibraryNames.push ("::name::");
-		::end::::end::
+		else preloadLibraryNames.push ("::library::");
 		
 	}
 	
@@ -72,34 +68,4 @@ import sys.FileSystem;
 }
 
 
-#if !display
-#if flash
-
-::foreach assets::::if (embed != false)::::if (type == "image")::@:keep @:bind #if display private #end class __ASSET__::flatName:: extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }::else::@:keep @:bind #if display private #end class __ASSET__::flatName:: extends ::flashClass:: { }::end::
-::end::::end::
-
-#elseif (desktop || cpp)
-
-::if (assets != null)::::foreach assets::::if (embed)::::if (type == "image")::@:image("::sourcePath::") #if display private #end class __ASSET__::flatName:: extends lime.graphics.Image {}
-::elseif (type == "sound")::@:file("::sourcePath::") #if display private #end class __ASSET__::flatName:: extends haxe.io.Bytes {}
-::elseif (type == "music")::@:file("::sourcePath::") #if display private #end class __ASSET__::flatName:: extends haxe.io.Bytes {}
-::elseif (type == "font")::@:font("::sourcePath::") #if display private #end class __ASSET__::flatName:: extends lime.text.Font {}
-::else::@:file("::sourcePath::") #if display private #end class __ASSET__::flatName:: extends haxe.io.Bytes {}
-::end::::end::::end::::end::
-::if (assets != null)::::foreach assets::::if (!embed)::::if (type == "font")::@:keep #if display private #end class __ASSET__::flatName:: extends lime.text.Font { public function new () { ::if (targetPath != null)::__fontPath = #if (ios || tvos) "assets/" + #end "::targetPath::";::else::::if (library != null)::__fontID = "::library:::::id::";::else::__fontID = "::id::";::end::::end:: name = "::fontName::"; super (); }}
-::end::::end::::end::::end::
-
-#else
-
-::if (assets != null)::::foreach assets::::if (type == "font")::@:keep @:expose('__ASSET__::flatName::') #if display private #end class __ASSET__::flatName:: extends lime.text.Font { public function new () { #if !html5 __fontPath = "::targetPath::"; #end name = "::fontName::"; super (); }}
-::end::::end::::end::
-
-#end
-
-#if (openfl && !flash)
-
-::if (assets != null)::::foreach assets::::if (type == "font")::@:keep @:expose('__ASSET__OPENFL__::flatName::') #if display private #end class __ASSET__OPENFL__::flatName:: extends openfl.text.Font { public function new () { ::if (embed)::var font = new __ASSET__::flatName:: (); src = font.src; name = font.name;::else::#if !html5 ::if (targetPath != null)::__fontPath = #if (ios || tvos) "assets/" + #end "::targetPath::";::else::::if (library != null)::__fontID = "::library:::::id::";::else::__fontID = "::id::";::end::::end:: #end name = "::fontName::";::end:: super (); }}
-::end::::end::::end::
-
-#end
-#end
+::images::
